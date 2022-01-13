6ebdf2b5e0a6e270d76568b6533a82032da7247b
Andres <andres.pintosantiso@gmail.com>
2022-01-13 12:20:44 -0400

    test

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,6 @@
 export const handler = async (): Promise<any> => {
   return {
     statusCode: 200,
-    body: JSON.stringify('Test deploy from github action with subfolder!!!'),
+    body: JSON.stringify('Test deploy from github action with subfolder!!!!'),
   };
 };
